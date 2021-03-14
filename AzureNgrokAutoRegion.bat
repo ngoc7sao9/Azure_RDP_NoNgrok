f3a6dc8cdd5b1916ae59df9f8c3abbed29f60a8b
ngoc7sao9 <27774949+ngoc7sao9@users.noreply.github.com>
2021-03-14 17:00:07 +0700

    Update AzureNgrokAutoRegion.bat

--- AzureNgrokAutoRegion.bat
+++ AzureNgrokAutoRegion.bat
@@ -2,7 +2,7 @@
 title Azure-Auto-Region
 
 echo Download all files...
-curl --silent -O https://raw.githubusercontent.com/ngoc7sao9/Azure_RDP_Hack/main/NGROK-CHECK.bat
+curl --silent -O https://raw.githubusercontent.com/ngoc7sao9/Azure_RDP_NoNgrok/main/NGROK-CHECK.bat
 
 echo Finished!
 NGROK-CHECK.bat
